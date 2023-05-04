bcab71df55393590932e9cf4073434c8a92811b0
aidanahram <52716460+aidanahram@users.noreply.github.com>
2023-05-03 23:43:03 -0400

    Update Scheduler.h

--- Scheduler.h
+++ Scheduler.h
@@ -1,10 +1,19 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include <bits/stdc++.h>
 #include <string>
 
+using namespace std;
 
 class Scheduler {
 	public: 
 		Scheduler();
 		
-		void readfile(std::string filepath);
-		
+		void readfile(std::string inputFile);
+		void printCourses();
+
+		unordered_set<string> courses;
 };
+
+#endif
